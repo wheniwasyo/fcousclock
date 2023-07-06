39a731d66b29b9d31f29bfc6e0610fac195b2f91
wheniwasyo <110074573+wheniwasyo@users.noreply.github.com>
2023-07-06 15:10:04 +0800

    Create fcousclock.py

---
+++ fcousclock.py
@@ -0,0 +1,13 @@
+import time
+
+def pomodoro_timer(minutes):
+    seconds = minutes * 60
+    while seconds > 0:
+        minutes_remaining = seconds // 60
+        seconds_remaining = seconds % 60
+        print(f"倒计时：{minutes_remaining:02d}:{seconds_remaining:02d}")
+        time.sleep(1)
+        seconds -= 1
+    print("专注时间结束！")
+
+pomodoro_timer(25)  # 设置专注时间为25分钟
